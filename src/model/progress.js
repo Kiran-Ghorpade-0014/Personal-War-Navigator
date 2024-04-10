db535d4722cfbe43179d486351bfc908e003a2ca
kiran ghorpade <kiranghorpade0014@gmail.com>
2024-04-11 01:56:36 +0530

    refactor : removed unused files

--- src/model/progress.js
+++
@@ -1,8 +0,0 @@
-export function createProgress(completed, total, completion_percentage) {
-  const progress = {};
-  progress.completed = completed;
-  progress.total = total;
-  progress.completion_percentage = completion_percentage;
-
-  return progress;
-}
--- src/model/resource.js
+++
@@ -1,8 +0,0 @@
-export function createResources(time, funds, assets) {
-  const resources = {};
-  resources.time = time;
-  resources.funds = funds;
-  resources.assets = assets;
-
-  return resources;
-}
